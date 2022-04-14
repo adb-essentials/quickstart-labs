2c5b35215f60c03f5a350cbd8a1e9142feee9039
furlong46 <furlong46>
2022-04-14 13:07:29 +0000

    Added screenshots

--- ADBQuickStartLabs/02 - SQL Analyst.sql
+++ ADBQuickStartLabs/02 - SQL Analyst.sql
@@ -68,6 +68,71 @@ dbutils.fs.rm('/mnt/adbquickstart/checkpoint', recurse=True)
 
 -- COMMAND ----------
 
+-- MAGIC %md
+-- MAGIC ### Databricks SQL is a DW and BI Engine directly on the Lakehouse  
+-- MAGIC <img src="https://publicimg.blob.core.windows.net/images/DBSQL1.png" width="1200">
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Open a new browser tab. In the lefthand navigation, change the persona switcher to SQL  
+-- MAGIC 
+-- MAGIC <img src="https://publicimg.blob.core.windows.net/images/DBSQL2.png" width="300">
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Compute in Databricks SQL is called SQL Endpoints  
+-- MAGIC SQL Endpoints are compute clusters optimized for DW and BI queries on the Lakehouse  
+-- MAGIC They are powered by Delta and Photon - a C++ vectorized engine that is really fast  
+-- MAGIC Photon is __*FREE*__ in Databricks SQL!
+-- MAGIC 
+-- MAGIC 
+-- MAGIC <img src="https://publicimg.blob.core.windows.net/images/DBSQL3.png" width="1200">
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Browse your databases and tables using Data Explorer
+-- MAGIC In Data Explorer you can see the objects you have access to  
+-- MAGIC For each table/view, you can view the size, schema, sample data, table details, and even version history of the table  
+-- MAGIC If you are an admin, you can even manage permissions to the databases and tables in Data Explorer  
+-- MAGIC 
+-- MAGIC 
+-- MAGIC <img src="https://publicimg.blob.core.windows.net/images/DBSQL4.png" width="1200">
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### The SQL Editor is powerful SQL IDE built directly into Databricks SQL
+-- MAGIC Browse and search for schema and table objects, view metadata, and explore tables/views  
+-- MAGIC Use functionality like intellisense, view past execution history, create data visualization capabilities, and even download data  
+-- MAGIC 
+-- MAGIC <img src="https://publicimg.blob.core.windows.net/images/DBSQL5.png" width="500">
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Queries are SQL code, results, and visualizations that you can save
+-- MAGIC Create SQL quieres, save them, and share them with your team  
+-- MAGIC View query results, or create data visualizations directly in the UI  
+-- MAGIC Schedule refreshes of your queries so that the results are always up to date  
+-- MAGIC 
+-- MAGIC <img src="https://publicimg.blob.core.windows.net/images/DBSQL6.png" width="1200">
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Step 0, Create Your Databases and Tables
+-- MAGIC If you didn't already complete the Data Engineering lab, you'll need to execute the Step 0 query  
+-- MAGIC Step 0 creates the database and tables for the SQL Analyst lab using SQL ingestion techniques.  It also optimizes and analyzes those tables   
+-- MAGIC Note that Databricks SQL is a DW engine that can be used for analysis, but also SQL based ETL routines like COPY INTO or CTAS (Create Table As Select)  
+-- MAGIC The purpose of this lab is to focus on the SQL analysis capabilities
+-- MAGIC 
+-- MAGIC <img src="https://publicimg.blob.core.windows.net/images/DBSQL7.png" width="1200">
+
+-- COMMAND ----------
+
 -- MAGIC %md 
 -- MAGIC ## Analyze Our Data for Patterns and Metrics
 -- MAGIC Data Analysts typically explore enterprise data for patterns, historical trends, and answering data-driven questions about the business. 
@@ -80,8 +145,4 @@ dbutils.fs.rm('/mnt/adbquickstart/checkpoint', recurse=True)
 -- MAGIC * What is the distribution of listening rates (% of songs listened to) among our subscribers? Does gender impact the listening time?
 -- MAGIC * Do subscribers who have been customers for a longer time use the platform more? Does it impact their likelihood to renew their subscription?
 -- MAGIC 
--- MAGIC All these questions can be answered with SQL against the data. Let's go through them one by one in Databricks SQL
-
--- COMMAND ----------
-
-
+-- MAGIC All these questions can be answered with SQL against the data. Let's go through them one by one in Databricks SQL using the queries already created. 
